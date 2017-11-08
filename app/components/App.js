44ec847d58e1250db7607038e858f28179077e46
Fan <Haoda.Fan@ontario.ca>
2017-11-08 17:14:42 -0500

    First Commit. Not working.

---
+++ app/components/App.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react=dom');
+
+module.exports = {
+  class mainComponent extends React.Component {
+    render() {
+      return <h1> ATTENTION UNIVERSE, THIS IS A REACT APP. </h1>;
+    }
+  }
+}
